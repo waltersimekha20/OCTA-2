58a8431007b1b8b6ebb48c5aa51aa8d3197899f5
Joe <jakatsa5@gmail.com>
2024-04-22 09:44:30 +0300

    updates

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,43 +1,64 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../style/Footer.css";
 const navStyles = {
-  display: 'flex',
-  backgroundColor: '#007bff',
-  padding: '10px',
+  display: "flex",
+  backgroundColor: "#007bff",
+  padding: "10px",
 };
 
 const linkStyles = {
-  textDecoration: 'none',
-  color: 'white',
-  margin: '0 10px',
-  padding: '8px',
-  borderRadius: '5px',
-  transition: 'background 0.3s',
+  textDecoration: "none",
+  color: "white",
+  margin: "0 10px",
+  padding: "8px",
+  borderRadius: "5px",
+  transition: "background 0.3s",
 };
 
 const activeLinkStyles = {
-  background: '#007bff',
+  background: "#007bff",
 };
 function Footer() {
   return (
-    <div style={navStyles} className='mt-4'>
+    <div style={navStyles} className="navbar">
       <NavLink to="/" exact style={linkStyles} activeStyle={activeLinkStyles}>
         Home
       </NavLink>
-      <NavLink to="/staffs" exact style={linkStyles} activeStyle={activeLinkStyles}>
+      <NavLink
+        to="/staffs"
+        exact
+        style={linkStyles}
+        activeStyle={activeLinkStyles}
+      >
         Staffs
       </NavLink>
-      <NavLink to="/patients" exact style={linkStyles} activeStyle={activeLinkStyles}>
+      <NavLink
+        to="/patients"
+        exact
+        style={linkStyles}
+        activeStyle={activeLinkStyles}
+      >
         Patients
       </NavLink>
-      <NavLink to="/appointments" exact style={linkStyles} activeStyle={activeLinkStyles}>
+      <NavLink
+        to="/appointments"
+        exact
+        style={linkStyles}
+        activeStyle={activeLinkStyles}
+      >
         Appointment
       </NavLink>
-      <NavLink to="/logout" exact style={linkStyles} activeStyle={activeLinkStyles}>
+      <NavLink
+        to="/logout"
+        exact
+        style={linkStyles}
+        activeStyle={activeLinkStyles}
+      >
         Log Out
       </NavLink>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,50 +1,63 @@
-import React, { useContext } from 'react'
-import { NavLink } from 'react-router-dom'
-import { AuthContext } from '../context';
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { AuthContext } from "../context";
 const navStyles = {
-  display: 'flex',
-  backgroundColor: '#007bff',
-  padding: '10px',
+  display: "flex",
+  backgroundColor: "tomato",
+  padding: "10px",
 };
 
 const linkStyles = {
-  textDecoration: 'none',
-  color: 'white',
-  margin: '0 10px',
-  padding: '8px',
-  borderRadius: '5px',
-  transition: 'background 0.3s',
+  textDecoration: "none",
+  color: "white",
+  margin: "0 10px",
+  padding: "8px",
+  borderRadius: "5px",
+  transition: "background 0.3s",
 };
 
 const activeLinkStyles = {
-  background: '#007bff',
+  background: "#007bff",
 };
 
 function Menu() {
-    const {logoutUser, userData} = useContext(AuthContext)
+  const { logoutUser, userData } = useContext(AuthContext);
   return (
-      <div style={navStyles}>
+    <div style={navStyles} className="menu">
       <NavLink to="/" style={linkStyles} activestyle={activeLinkStyles}>
         Home
       </NavLink>
-      <NavLink to="/staffs"  style={linkStyles} activestyle={activeLinkStyles}>
+      <NavLink to="/staffs" style={linkStyles} activestyle={activeLinkStyles}>
         Staffs
       </NavLink>
       <NavLink to="/patients" style={linkStyles} activestyle={activeLinkStyles}>
         Patients
       </NavLink>
-      <NavLink to="/appointments" style={linkStyles} activestyle={activeLinkStyles}>
+      <NavLink
+        to="/appointments"
+        style={linkStyles}
+        activestyle={activeLinkStyles}
+      >
         Appointment
       </NavLink>
-      <NavLink to="/logout"  style={linkStyles} activestyle={activeLinkStyles} onClick={() => logoutUser()}>
+      <NavLink
+        to="/logout"
+        style={linkStyles}
+        activestyle={activeLinkStyles}
+        onClick={() => logoutUser()}
+      >
         Log Out
       </NavLink>
       <div>
-       <NavLink aria-disabled="true" style={linkStyles}>{userData.username}</NavLink>
-       <NavLink aria-disabled="true" style={linkStyles}>{userData.email}</NavLink>
+        <NavLink aria-disabled="true" style={linkStyles}>
+          {userData.username}
+        </NavLink>
+        <NavLink aria-disabled="true" style={linkStyles}>
+          {userData.email}
+        </NavLink>
       </div>
     </div>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
