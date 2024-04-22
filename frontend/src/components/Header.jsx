b1e3beed107540c0ddacd0113848305d5931f431
Joe <jakatsa5@gmail.com>
2024-04-22 11:33:56 +0300

    updates

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Header() {
-  return (
-    <div>
-      <h1 className="text-left ml-6 text-3xl font-bold mt-6">OCTANURSE</h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Header;
